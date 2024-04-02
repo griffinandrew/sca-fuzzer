b2d865d710d154085772a3706ba63d97897b92d8
griffinandrew <gaheyrich@gmail.com>
2024-04-02 18:53:29 -0400

    modify for 25%

--- input_gen_data.sh
+++ input_gen_data.sh
@@ -2,7 +2,7 @@
 
 COMMAND="rvzr fuzz -s base.json -n 1000 -i 25 -c config.yaml -w ./violations"
 
-FILE="output_33_mutate.txt"
+FILE="output_25_mutate.txt"
 
 for ((i=1; i<=15; i++)); do
     echo "************************"
