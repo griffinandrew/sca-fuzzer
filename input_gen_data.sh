536c1fbd13a4693646b154b0ead0e678aea818db
griffinandrew <gaheyrich@gmail.com>
2024-04-02 15:51:00 -0400

    add script to automate data collection

---
+++ input_gen_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COMMAND="rvzr fuzz -s base.json -n 100 -i 10  -c config.yaml -w ./violations"
+
+FILE="output.txt"
+
+
+for ((i=1; i<=15; i++)); do
+    echo "************************"
+    echo "running iter $i..."
+    echo "************************" >> "$FILE"
+    echo "iter $i:" >> "$FILE"
+    $COMMAND >> "$FILE" 2>&1
+    echo "iter $i complete"
+done
+
+echo "complete"
