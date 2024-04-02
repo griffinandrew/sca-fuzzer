ac46ff72543926269bd8412e299b3a2172165d3f
griffinandrew <gaheyrich@gmail.com>
2024-04-02 15:14:47 -0400

    experimenting with when to mutate and when to replace randomly

--- src/input_generator.py
+++ src/input_generator.py
@@ -66,11 +66,11 @@ class InputGeneratorCommon(InputGenerator):
                 else:
                     #only do these mutations occasionally to cut on cost and have diversity of input
                     #perhaps this could be more sophisticated, but for now this is fine
-                    if random.randint(0, 1) == 0:
-                        t_inputs = self.get_idxs_with_taint(inputs, taints, i)
-                        if (len(t_inputs) > 2): 
-                            mutated_input = self.mutate_improved(inputs, taints, i, t_inputs) # this will be slow if need to find indexs and shit everytime (it is slow)
-                            new_input[j] = mutated_input #i think
+                    #if random.randint(0, 1) == 0:
+                    t_inputs = self.get_idxs_with_taint(inputs, taints, i)
+                    if (len(t_inputs) > 2): 
+                        mutated_input = self.mutate_improved(inputs, taints, i, t_inputs) # this will be slow if need to find indexs and shit everytime (it is slow)
+                        new_input[j] = mutated_input #i think
 
             new_inputs.append(new_input)
 
