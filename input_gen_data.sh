13b324fc69493c9cca9678d86c493b9083f7e230
griffinandrew <gaheyrich@gmail.com>
2024-04-02 17:48:33 -0400

    set for 100 mutuate

--- input_gen_data.sh
+++ input_gen_data.sh
@@ -2,7 +2,7 @@
 
 COMMAND="rvzr fuzz -s base.json -n 1000 -i 25  -c config.yaml -w ./violations"
 
-FILE="output_50_mutate.txt"
+FILE="output_100_mutate.txt"
 
 for ((i=1; i<=15; i++)); do
     echo "************************"
