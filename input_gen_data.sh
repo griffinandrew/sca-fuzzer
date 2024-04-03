5726fb221546334dac16e07c5f2d182d0678858b
griffinandrew <gaheyrich@gmail.com>
2024-04-02 23:13:20 -0400

    fix extned

--- input_gen_data.sh
+++ input_gen_data.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-COMMAND="rvzr fuzz -s base.json -n 1000 -i 25 -c config.yaml -w ./violations"
+COMMAND="rvzr fuzz -s base.json -n 500 -i 25 -c config.yaml -w ./violations"
 
 FILE="output_dumb_50_inputs.txt"
 
 for ((i=1; i<=15; i++)); do
-    echo "************************\n"
+    echo "************************"
     echo "running iter $i..."
-    echo "************************\n" >> "$FILE"
+    echo "************************" >> "$FILE"
     echo "iter $i:\n" >> "$FILE"
     $COMMAND >> "$FILE" 2>&1
-    echo "************************\n" >> "$FILE"
+    echo "************************" >> "$FILE"
     echo "iter $i complete"
-    echo "************************\n" >> "$FILE"
+    echo "************************" >> "$FILE"
 done
 
-echo "************************\n"
-echo "************************\n"
-echo "************************\n"
+echo "************************"
+echo "************************"
+echo "************************"
 echo "DONE"
-echo "************************\n"
-echo "************************\n"
-echo "************************\n"
+echo "************************"
+echo "************************"
+echo "************************"
