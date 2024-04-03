71c6013cf47a06c784f3940059680cd179eff598
griffinandrew <gaheyrich@gmail.com>
2024-04-02 20:20:14 -0400

    update for baseline 100 input

--- input_gen_data.sh
+++ input_gen_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-COMMAND="rvzr fuzz -s base.json -n 1000 -i 25 -c config.yaml -w ./violations"
+COMMAND="rvzr fuzz -s base.json -n 1000 -i 50 -c config.yaml -w ./violations"
 
 #FILE="output_100_mutate_100_inputs.txt"
-FILE="output_baseline_25_inputs.txt"
+FILE="output_baseline_100_inputs.txt"
 
 for ((i=1; i<=15; i++)); do
     echo "************************"
