15ce039693798489259595dfd4775c0e8c118769
griffinandrew <gaheyrich@gmail.com>
2024-04-02 22:49:56 -0400

    cleanup input_gen

--- input_gen_data.sh
+++ input_gen_data.sh
@@ -5,12 +5,20 @@ COMMAND="rvzr fuzz -s base.json -n 1000 -i 25 -c config.yaml -w ./violations"
 FILE="output_dumb_50_inputs.txt"
 
 for ((i=1; i<=15; i++)); do
-    echo "************************"
+    echo "************************\n"
     echo "running iter $i..."
-    echo "************************" >> "$FILE"
-    echo "iter $i:" >> "$FILE"
+    echo "************************\n" >> "$FILE"
+    echo "iter $i:\n" >> "$FILE"
     $COMMAND >> "$FILE" 2>&1
+    echo "************************\n" >> "$FILE"
     echo "iter $i complete"
+    echo "************************\n" >> "$FILE"
 done
 
-echo "complete"
+echo "************************\n"
+echo "************************\n"
+echo "************************\n"
+echo "DONE"
+echo "************************\n"
+echo "************************\n"
+echo "************************\n"
